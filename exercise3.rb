c4f157f33eb78d43b4c82911d7c580ab968ea06d
Stephen Chang <stephenycchang@gmail.com>
2014-03-25 13:42:54 -0400

    Added age question feature

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,12 @@
+name = true
+
 puts "What is your name?"
+	name = gets.chomp
+puts "Hi #{name}"
+
+you = true
+puts "How old are you?"
+	you = gets.chomp.to_i
+	year = 2014.to_i	
+puts "You are born in #{year - you}"
+
