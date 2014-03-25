86de88369ece56a5bf39e3e439b1065920eb04d6
Stephen Chang <stephenycchang@gmail.com>
2014-03-25 13:25:40 -0400

    Did exercise 2 calculation question

--- exercise2.rb
+++ exercise2.rb
@@ -1,2 +1,8 @@
 #How would you calculate a good tip for a 55 dollar meal?
 puts 55 * 1.15
+#Try adding a sting and an interger. What happens? Find a way to convert the integer so that it works and use puts to print the answer onscreen.
+puts "55" + 5.to_s
+#calculator
+a = 45628
+b = 7839
+puts "The value of a * b is #{a * b}"
