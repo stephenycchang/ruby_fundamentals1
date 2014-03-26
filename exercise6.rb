532272ae51238a83fe060f5165b056ce219509c0
Stephen Chang <stephenycchang@gmail.com>
2014-03-26 09:42:22 -0400

    Added Output for rice

--- exercise6.rb
+++ exercise6.rb
@@ -1,2 +1,14 @@
+#Original Array
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+#Printing each grocery list out with an * in front
 grocery_list.each { |i| print "*" + i.to_s }
+
+#Adding rice to grocery_list
+grocery_list << "rice"
+
+#Adding Output method
+def new_grocery_list(arr)
+  arr.each{ |i| print "*" + i.to_s}
+  
+end
