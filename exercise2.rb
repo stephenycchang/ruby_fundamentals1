2a76e07dfe2258ee3e40192cf280c51315d41ea9
Stephen Chang <stephenycchang@gmail.com>
2014-03-26 15:54:52 -0400

    Optimized exercise 2

--- exercise2.rb
+++ exercise2.rb
@@ -1,5 +1,5 @@
 #How would you calculate a good tip for a 55 dollar meal?
-puts 55 * 1.15
+puts (55 * 1.15).round
 #Try adding a sting and an interger. What happens? Find a way to convert the integer so that it works and use puts to print the answer onscreen.
 puts "55" + 5.to_s
 #calculator
