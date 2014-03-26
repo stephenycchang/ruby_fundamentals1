da05849a43d1a30e750f0ecb5f5b6a909c22bc98
Stephen Chang <stephenycchang@gmail.com>
2014-03-26 13:37:33 -0400

    Finished exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,14 +1,30 @@
 #Original Array
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-#Printing each grocery list out with an * in front
-grocery_list.each { |i| print "*" + i.to_s }
+def new_list(arr)
+  arr.each { |i| puts "* " + i.to_s}
+end
 
 #Adding rice to grocery_list
 grocery_list << "rice"
 
-#Adding Output method
-def new_grocery_list(arr)
-  arr.each{ |i| print "*" + i.to_s}
-  
+new_list(grocery_list)
+
+puts grocery_list.length
+
+if grocery_list.include?("bananas")
+  puts "You don't need to pick up bananas."
+else
+  puts "You need to pick up bananas!"
 end
+
+puts grocery_list[1]
+
+grocery_list.sort!
+
+new_list(grocery_list)
+
+grocery_list.delete("salmon")
+
+new_list(grocery_list)
+
